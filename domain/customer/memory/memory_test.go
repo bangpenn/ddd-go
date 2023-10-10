2cc0ce8df054322c3d0e6d3ecea2e686dca395d2
EFENDI <fendikrama56@gmail.com>
2023-10-10 08:34:15 +0700

    menambahkan package baru

---
+++ domain/customer/memory/memory_test.go
@@ -0,0 +1,52 @@
+package memory
+
+import (
+	"github.com/bangpenn/ddd-go/aggregate"
+	"github.com/google/uuid"
+	"github.com/bangpenn/ddd-go/domain/customer"
+	"testing"
+)
+
+func TestMemory_GetCustom(t *testing.T) {
+	type testCase struct {
+		name         string
+		id           uuid.UUID
+		exepectedErr error
+	}
+
+	cust, err := aggregate.NewCustomer("rizal")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id := cust.GetID()
+
+	repo := MemoryRepository{
+		customers: map[uuid.UUID]aggregate.Customer{
+			id: cust,
+		},
+	}
+
+	testCases := []testCase{
+		{
+			name: "no customer by id",
+			id: uuid.MustParse("f47ac10b-58cc-0372-8567-0e02b2c3d479"),
+			exepectedErr: customer.ErrCustomerNotFound,
+		}, {
+			name: "customer by id",
+			id: id
+			exepectedErr: nil,
+
+		}
+	}
+	for _, tc := range testCase{
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := repo.Get(tc.id)
+
+			if !errors.Is(err, tc.exepectedErr) {
+				t.Errorf("expected error %v, got %v", tc.exepectedErr, err)
+			}
+		})
+	}
+
+}
